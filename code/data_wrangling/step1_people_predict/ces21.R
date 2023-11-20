a6b25f1001f90a07be3e2cabae9959d55dd8202e
sarahjanevincent <141277350+sarahjanevincent@users.noreply.github.com>
2023-11-20 17:51:06 -0500

    people pred CES21

--- code/data_wrangling/step1_people_predict/ces21.R
+++ code/data_wrangling/step1_people_predict/ces21.R
@@ -26,8 +26,46 @@ table(Clean$pol_sophis)
 
 
 ## riding prediction ---------------------------------------------------------------------
-Clean$people_pred <- NA
 
+## People predict Libéral
+table(Raw$cps21_win_local_1)
+Clean$people_pred_liberal <- NA
+Clean$people_pred_liberal <- as.numeric(Raw$cps21_win_local_1)
+Clean$people_pred_liberal[as.numeric(Raw$cps21_win_local_1) == -99] <- NA
+Clean$people_pred_liberal <- Clean$people_pred_liberal / 100
+table(Clean$people_pred_liberal)
+
+## People predict conservateur
+table(Raw$cps21_win_local_2)
+Clean$people_pred_conservateur <- NA
+Clean$people_pred_conservateur <- as.numeric(Raw$cps21_win_local_2)
+Clean$people_pred_conservateur[as.numeric(Raw$cps21_win_local_2) == -99] <- NA
+Clean$people_pred_conservateur <- Clean$people_pred_conservateur / 100
+table(Clean$people_pred_conservateur)
+
+## People predict NPD
+table(Raw$cps21_win_local_3)
+Clean$people_pred_npd <- NA
+Clean$people_pred_npd <- as.numeric(Raw$cps21_win_local_3)
+Clean$people_pred_npd[as.numeric(Raw$cps21_win_local_3) == -99] <- NA
+Clean$people_pred_npd <- Clean$people_pred_npd / 100
+table(Clean$people_pred_npd)
+
+## People predict Bloc québécois
+table(Raw$cps21_win_local_4)
+Clean$people_pred_blocqueb <- NA
+Clean$people_pred_blocqueb <- as.numeric(Raw$cps21_win_local_4)
+Clean$people_pred_blocqueb[as.numeric(Raw$cps21_win_local_4) == -99] <- NA
+Clean$people_pred_blocqueb <- Clean$people_pred_blocqueb / 100
+table(Clean$people_pred_blocqueb)
+
+## People predict Parti vert
+table(Raw$cps21_win_local_5)
+Clean$people_pred_partivert <- NA
+Clean$people_pred_partivert <- as.numeric(Raw$cps21_win_local_5)
+Clean$people_pred_partivert[as.numeric(Raw$cps21_win_local_5) == -99] <- NA
+Clean$people_pred_partivert <- Clean$people_pred_partivert / 100
+table(Clean$people_pred_partivert)
 
 # Save Clean to a rds dataset ---------------------------------------------
 
