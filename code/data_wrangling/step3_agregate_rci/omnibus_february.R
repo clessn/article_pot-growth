ead232d9df39e35b43619a6f3baff40fef996e0a
hubcad25 <hubertcadieux@gmail.com>
2023-11-22 10:14:06 -0500

    riding omnibus february

---
+++ code/data_wrangling/step3_agregate_rci/omnibus_february.R
@@ -0,0 +1,45 @@
+# Load packages ----------------------------------------------------------------
+
+
+# Load raw data -----------------------------------------------------------
+Raw <- haven::read_sav("_SharedFolder_article_pot-growth/data/lake/omnibus/february/february.Sav")
+
+# Create empty clean dataframe --------------------------------------------
+Clean <- data.frame(id = 1:nrow(Raw), # id of the respondent
+                    source_id = "omnibus_february", # id of the survey
+                    year = 2022, # year of the survey
+                    level = "prov_qc") # fed_can or prov_qc
+
+# Clean variables ---------------------------------------------------------
+
+## gender ------------------------------------------------------------------
+
+## age ------------------------------------------------------------------
+
+## language ------------------------------------------------------------------
+
+## riding ------------------------------------------------------------------
+
+#### Load data from article_riding_volatility to get riding
+riding_volatility_df <- readRDS("_SharedFolder_article_pot-growth/data/lake/riding_volatility_data.rds") %>% 
+  # filter for january only
+  filter(source_id == "february")
+table(riding_volatility_df$riding_id)
+
+## Create riding_id column in Clean
+Clean$riding_id <- riding_volatility_df$riding_id
+
+## Load riding names (to join on riding_id)
+riding_names_df <- readRDS("_SharedFolder_article_pot-growth/data/warehouse/dimensions/prov_ridings/data.rds") %>% 
+  select(riding_id, riding_name)
+
+### Join riding_name on riding_id
+Clean <- left_join(Clean, riding_names_df, by = "riding_id")
+
+
+## RCI ---------------------------------------------------------------------
+
+
+# Save Clean to a rds dataset ---------------------------------------------
+
+saveRDS(Clean, "_SharedFolder_article_pot-growth/data/warehouse/step3_agregate_rci/separated/omnibus_february.rds")
